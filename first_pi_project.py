66ef6be15aa2437174fe3d9d1ac198bd30f099fa
FurryLover69 <48027438+FurryLover69@users.noreply.github.com>
2019-07-19 19:48:32 +0200

    Add files via upload

---
+++ first_pi_project.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+# pin definitions
+pwmPin = 18
+ledPin = 23
+butPin = 17
+
+duty = 75
+
+#setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin,GPIO.OUT)
+GPIO.setup(pwnPin,GPIO.OUT)
+GPIO.setup(butPin,GPIO.IN, pull_up_down = GPIO.PUD_UP)
+pwm = GPIO.PWM(pwmPIN, 200)
+GPIO.output(ledPin, GPIO.LOW)
+pwm.start(duty)
+
+try:
+    while 1:
+        # if button not pressed do something
+        if GPIO.input(butPin):
+            pwm.ChangeDutyCycle(duty)
+            GPIO.output(ledPin, GPIO.LOW)
+            
+        else:
+            pwm.ChangeDutyCycle(duty)
+            GPIO.output(ledPin, GPIO.HIGH)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(100-duty)
+            GPIO.output(ledPin, GPIO.LOW)
+            time.sleep(0.5)
+ 
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
+   
