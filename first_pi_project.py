7fbf8cf8465c2ca781ff875bcc80203c236bf94c
FurryLover69 <48027438+FurryLover69@users.noreply.github.com>
2019-07-19 20:09:02 +0200

    Add files via upload

--- first_pi_project.py
+++ first_pi_project.py
@@ -11,9 +11,9 @@ duty = 75
 #setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin,GPIO.OUT)
-GPIO.setup(pwnPin,GPIO.OUT)
+GPIO.setup(pwmPin,GPIO.OUT)
 GPIO.setup(butPin,GPIO.IN, pull_up_down = GPIO.PUD_UP)
-pwm = GPIO.PWM(pwmPIN, 200)
+pwm = GPIO.PWM(pwmPin, 200)
 GPIO.output(ledPin, GPIO.LOW)
 pwm.start(duty)
 
